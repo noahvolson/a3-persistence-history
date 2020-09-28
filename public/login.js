639cb0870b822d59537b9b32406a7448988cc504
noahvolson <noah.v.olson@gmail.com>
2020-09-28 01:48:18 -0400

    Basic structure established

---
+++ public/login.js
@@ -0,0 +1,4 @@
+document.getElementById("delete").onclick = function (e) {
+    e.preventDefault();
+    console.log("CLICKED!")
+}
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser"); 
+
+// added middleware
+const bodyParser = require("body-parser");  // 1
+const favicon = require("serve-favicon");   // 2
+const helmet = require("helmet");           // 3
+const morgan = require("morgan");           // 4 //TODO Only log user login requests
+
 const app = express();
 
 const dreams = [
@@ -8,10 +14,14 @@ const dreams = [
   "Wash the dishes"
 ];
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(helmet({contentSecurityPolicy: false}))
 
+app.use(morgan('combined'));
+
+app.use(favicon(__dirname + "/public/favicon.ico"));
+
+// make all the files in 'public' available
+app.use(express.static("public"));
 
 // for when /index.html not specified
 app.get("/", (request, response) => {
